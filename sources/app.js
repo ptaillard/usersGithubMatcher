37c6a259ed84879df70793b9eb5ed00984ed0a04
pierre.taillard <pierre_taillard@yahoo.fr>
2015-12-12 14:44:05 +0100

    empty app

---
+++ sources/app.js
@@ -0,0 +1,8 @@
+/* global angular */
+(function() {
+    'use strict';
+
+    angular.module('vendors', ['ngResource', 'ngAria', 'ngAnimate', 'ngMaterial']);
+    angular.module('usersGithubMatcherApp', ['vendors']);
+
+})();
